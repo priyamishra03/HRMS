0facef25c17649b15b2216e0c2c27215d7eed238
NotRuvik <103015502+NotRuvik@users.noreply.github.com>
2024-05-21 04:10:35 +0530

    Update sidebar.php
    Deprecated #01

--- application/views/backend/sidebar.php
+++ application/views/backend/sidebar.php
@@ -70,29 +70,7 @@
                                 <li><a href="<?php echo base_url(); ?>leave/Leave_report"> Report </a></li> -->
                             </ul>
                         </li>
-                        <!-- <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-briefcase-check"></i><span class="hide-menu">Project </span></a>
-                            <ul aria-expanded="false" class="collapse">
-                                <li><a href="<?php echo base_url(); ?>Projects/All_Projects">Projects </a></li>
-                                <li><a href="<?php echo base_url(); ?>Projects/All_Tasks"> Task List </a></li>
-                                 <li><a href="<?php echo base_url(); ?>Projects/Field_visit"> Field Visit</a></li>
-                            </ul>
-                        </li> -->
-                        <!-- <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-currency-usd"></i><span class="hide-menu">Loan </span></a>
-                            <ul aria-expanded="false" class="collapse">
-                                <li><a href="<?php echo base_url(); ?>Loan/View"> Grand Loan </a></li>
-                                <li><a href="<?php echo base_url(); ?>Loan/installment"> Loan Installment</a></li>
-                            </ul>
-                        </li> -->
-                        <!-- <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-barcode"></i><span class="hide-menu">Assets </span></a>
-                            <ul aria-expanded="false" class="collapse">
-                                <li><a href="<?php echo base_url(); ?>Logistice/Assets_Category"> Assets Category </a></li>
-                                <li><a href="<?php echo base_url(); ?>Logistice/All_Assets"> Asset List </a></li>
-                                <li><a href="<?php #echo base_url(); ?>Logistice/View"> Logistic Support List </a></li>
-                                <li><a href="<?php echo base_url(); ?>Logistice/logistic_support"> Logistic Support </a></li>
-                            </ul>
-                        </li> -->
-                        <!-- <li> <a href="<?php echo base_url()?>notice/All_notice" ><i class="mdi mdi-treasure-chest"></i><span class="hide-menu">Notice <span class="hide-menu"></a></li> -->
-                        <!--<li> <a href="<?php echo base_url(); ?>settings/Settings" ><i class="mdi mdi-settings"></i><span class="hide-menu">Settings <span class="hide-menu"></a></li> -->
+                        
                         <?php } ?>
                     </ul>
                 </nav>
